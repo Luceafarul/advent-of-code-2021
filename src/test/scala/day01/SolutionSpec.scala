c042d948e67e0fb8ab0db38d53c7ccef5105a129
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-12-01 21:12:10 +0200

    Add task one of day one.

---
+++ src/test/scala/day01/SolutionSpec.scala
@@ -0,0 +1,14 @@
+package day01
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class SolutionSpec extends AnyWordSpec with Matchers {
+  "A Solution" should {
+    "return count of increases depth measurement" in {
+      val measurements = List(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+
+      Solution.depthMeasurementIncreasesCounter(measurements) shouldBe 7
+    }
+  }
+}
