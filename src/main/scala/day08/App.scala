df0df957262a2521775df4d45056496295ae6ccd
Yaroslav Sydorenko <yaroslav.leo@gmail.com>
2021-12-08 23:39:45 +0200

    Add solution for first part of day 8.

--- src/main/scala/day08/App.scala
+++ src/main/scala/day08/App.scala
@@ -1,14 +1,17 @@
 package day08
 
 object App extends App {
-  val source = io.Source.fromResource("day07.txt")
+  val source = io.Source.fromResource("day08.txt")
 
   val lines = source.getLines().toList
 
-  val positions = lines.flatMap(_.split(",").map(_.toInt))
+  val numbers = lines.flatMap { s =>
+    val Array(_, numbers) = s.split("\\|")
+    numbers.split(" ").map(_.trim).filter(s => !s.isBlank).toList
+  }
 
   println(
-    s": ${}\n" + ""
+    s"Count of easy digits: ${Solution.filterEasyDigits(numbers).size}\n" + ""
   )
 
   source.close()
--- src/test/scala/day08/SolutionSpec.scala
+++ src/test/scala/day08/SolutionSpec.scala
@@ -5,6 +5,21 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class SolutionSpec extends AnyWordSpec with Matchers {
   "A Solution" should {
-    "" in {}
+    val input = List(
+      "fdgacbe cefdb cefbgd gcbe",
+      "fcgedb cgb dgebacf gc",
+      "cg cg fdcagb cbg",
+      "efabcd cedba gadfec cb",
+      "gecf egdcabf bgf bfgea",
+      "gebdcfa ecba ca fadegcb",
+      "cefg dcbef fcge gbcadfe",
+      "ed bcgafe cdgba cbgef",
+      "gbdfcae bgc cg cgb",
+      "fgae cfgab fg bagce"
+    ).flatMap(_.split(" "))
+
+    "correct evaluate count of numbers 1, 4, 7, 8" in {
+      Solution.filterEasyDigits(input).size shouldBe 26
+    }
   }
 }
